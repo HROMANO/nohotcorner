838988470b5f1cf9fef9ddc3ae87e218e87ef18d
HROMANO <azuri@free.fr>
2013-09-03 14:30:26 +0200

    Patch for multiscreen
    Should disable hotcorners on multiscreen (not tested). Patch submited by PierreBdR

--- extension.js
+++ extension.js
@@ -1,12 +1,18 @@
+/* Version 3 :
+- disables all topleft hot corners on multiscreens [PierreBdR]
+*/
+
 const Main = imports.ui.main;
 
 function init(metadata) {
 }
 
-function enable() {
-   Main.panel._activitiesButton._hotCorner._corner.hide();
-}
+function enable() { 
+	Main.panel._activitiesButton._hotCorner._corner.hide(); 
+    Main.layoutManager._hotCorners.forEach(function(hotCorner) { hotCorner._corner.hide(); }); 
+} 
 
-function disable() {
-   Main.panel._activitiesButton._hotCorner._corner.show();
+function disable() { 
+    Main.panel._activitiesButton._hotCorner._corner.show(); 
+    Main.layoutManager._hotCorners.forEach(function(hotCorner) { hotCorner._corner.show(); }); 
 }
