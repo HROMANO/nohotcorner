d436dccdf185afc2c40f4c73efc71355e8100872
HROMANO <azuri@free.fr>
2013-09-03 14:32:49 +0200

    Update extension.js for 3.6
    Update for gnome-shell 3.6

--- extension.js
+++ extension.js
@@ -1,18 +1,26 @@
-/* Version 3 :
+/* Version 4 :
+- update for 3.6
+
+Version 3 :
 - disables all topleft hot corners on multiscreens [PierreBdR]
 */
 
 const Main = imports.ui.main;
 
-function init(metadata) {
+function init() {
 }
 
 function enable() { 
-	Main.panel._activitiesButton._hotCorner._corner.hide(); 
-    Main.layoutManager._hotCorners.forEach(function(hotCorner) { hotCorner._corner.hide(); }); 
+   Main.panel.statusArea['activities'].hotCorner._corner.hide();
+   
+   /* Should disable hotcorners in multiscreen configurations (thanks PierreBdR).
+   Can't try as I only have one screen and the array _hotCorners is always empty
+   (though it's a bit strange). */
+   Main.layoutManager._hotCorners.forEach(function(hotCorner) { hotCorner._corner.hide(); });
 } 
 
 function disable() { 
-    Main.panel._activitiesButton._hotCorner._corner.show(); 
-    Main.layoutManager._hotCorners.forEach(function(hotCorner) { hotCorner._corner.show(); }); 
+   Main.panel.statusArea['activities'].hotCorner._corner.show();
+   Main.layoutManager._hotCorners.forEach(function(hotCorner) { hotCorner._corner.show(); }); 
 }
+
